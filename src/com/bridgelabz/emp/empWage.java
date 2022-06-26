bb3ffe0bb07656bb8aded5712915194f0e73628f
Him5696 <himanshi1252@gmail.com>
2022-06-26 16:12:32 +0530

    Added:  Employee wages

--- src/com/bridgelabz/emp/empWage.java
+++ src/com/bridgelabz/emp/empWage.java
@@ -4,11 +4,15 @@ public class empWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage computation Problem");
         int isFullTime = 1;
-        double employeeCheck = Math.floor (Math.random() * 10) % 2;
+        int EMP_WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOUR = 8;
+        int employeeCheck = (int) (Math.random() * 10) % 2;
         if (employeeCheck == isFullTime) {
             System.out.println("Employee is present");
+            int empWage =FULL_DAY_HOUR * EMP_WAGE_PER_HOUR;
+            System.out.println("Daily wage of employee is: " + empWage);
         }else {
-            System.out.println("Employee is Absent");
+            System.out.println("Employee is Absent: No wage");
         }
     }
 }
