d18b0fa86e182aff7b8dd8d8a4427728802ed2be
Him5696 <himanshi1252@gmail.com>
2022-06-26 16:07:47 +0530

    Added:  Employee Attendance

--- src/com/bridgelabz/emp/empWage.java
+++ src/com/bridgelabz/emp/empWage.java
@@ -3,5 +3,12 @@ package com.bridgelabz.emp;
 public class empWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage computation Problem");
+        int isFullTime = 1;
+        double employeeCheck = Math.floor (Math.random() * 10) % 2;
+        if (employeeCheck == isFullTime) {
+            System.out.println("Employee is present");
+        }else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
