49736df6bbba34888974d755f8653e44da4083a5
Him5696 <himanshi1252@gmail.com>
2022-06-26 16:34:25 +0530

    Added: Wages For a Month

--- src/com/bridgelabz/emp/empWage.java
+++ src/com/bridgelabz/emp/empWage.java
@@ -6,23 +6,26 @@ public class empWage {
         int IS_FULL_TIME = 1;
         int IS_PART_TIME = 2;
         int EMP_WAGE_PER_HOUR = 20;
-        int employeeCheck =(int)(Math.random() * 10) % 3;
+        int WORKING_DAY_MONTH = 20;
+        int employeeCheck = (int) (Math.random() * 10) % 3;
 
-        switch (employeeCheck){
-            case 1:
-                System.out.println("Employee is full time");
-                EMP_PER_HOUR=8;
-                break;
-            case 2:
-                System.out.println("Employee is part time");
-                EMP_PER_HOUR = 4;
-                break;
-            default:
-                System.out.println("Employee is absent");
-                EMP_PER_HOUR=0;
-                break;
+            switch (employeeCheck) {
+                case 1:
+                    System.out.println("Employee is full time");
+                    EMP_PER_HOUR = 8;
+                    break;
+                case 2:
+                    System.out.println("Employee is part time");
+                    EMP_PER_HOUR = 4;
+                    break;
+                default:
+                    System.out.println("Employee is absent");
+                    EMP_PER_HOUR = 0;
+                    break;
+            }
+        for(int i = 1; i <=WORKING_DAY_MONTH; i++) {
         }
-        int empWage = EMP_PER_HOUR  * EMP_WAGE_PER_HOUR;
+        int empWage = EMP_PER_HOUR * EMP_WAGE_PER_HOUR * WORKING_DAY_MONTH;
         System.out.println("Employee Wages is: " + empWage);
 
     }
